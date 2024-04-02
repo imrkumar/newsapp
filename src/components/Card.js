dc7d4d75f1c0e7b28e600f72081f105902e5786e
Rahul Kumar <rahulkumar981417@gmail.com>
2024-04-02 19:58:17 +0530

    news api work

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 
 const Card = () => {
-  //   let url =
-  //     "https://newsapi.org/v2/everything?q=tesla&from=2024-03-01&sortBy=publishedAt&apiKey=0b6d9920db8d436ea57538343633cd7c";
+  // let url =
+  //   "https://newsapi.org/v2/everything?q=cricket&from=2024-03-02&sortBy=publishedAt&apiKey=0b6d9920db8d436ea57538343633cd7c";
   let url = "https://fakestoreapi.com/products";
   let [data, setData] = useState([]);
   function getNews(url) {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
+        // let { articles } = data;
+        // setData(articles);
         setData(data);
       });
   }
@@ -22,9 +24,19 @@ const Card = () => {
         return (
           <div className="card w-25 m-3 ">
             <div className="card-header d-flex justify-content-center">
-              <img src={item.image} className="w-75  card-img-height" alt="img"/>
+              <img
+                src={item.image}
+                className="w-75  card-img-height"
+                alt="img"
+              />
             </div>
-            <div className="card-body">{item.title}</div>
+            <div className="card-body">{item.title}
+          
+            </div>
+            {/* <div className="card-footer d-flex justify-content-between">
+              <span><small className="text-muted">{item.publishedAt}</small></span>
+              <span><a href={item.url} className="btn btn-danger">Read</a></span>
+            </div> */}
           </div>
         );
       })}
